335ada137b3d25a5df5f51a1fd97837f55fc1f99
Omkarfastcoder <99040673+Omkarfastcoder@users.noreply.github.com>
2022-06-22 18:40:34 +0530

    Update loan.js

--- js/loan.js
+++ js/loan.js
@@ -3,21 +3,43 @@ function NameValidation() {
     var full_name = document.getElementById("fn").value;
     var ind = full_name.indexOf(' ');
     if (!RegName.test(full_name)) {
+        document.getElementById('submit').disabled = true;
         alert('name should contain 2 words having alphabets and 4 letters each');
     }
     else if (!ind > 3) {
+        document.getElementById('submit').disabled = true;
         alert('name should contain 2 words having alphabets and 4 letters each');
+    }
+    else {
+        enableSubmitButton();
     }
     
     var [first, last] = full_name.split(' ');
     localStorage.setItem("nameout", first);
+    
 }
 
+function enableSubmitButton()
+{
+    if((document.getElementById("fn").value.length > 0) &&
+       (document.getElementById("email").value.length > 0) &&
+       (document.getElementById("pan").value.length > 0) &&
+       (document.getElementById("loanAmt").value.length > 0) &&
+      document.getElementById("telephone").value.length > 0) &&)
+       {
+       document.getElementById('submit').disabled = false;
+       }
+}
+}
 function EmailValidation() {
     var regMail = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     var mailId = document.getElementById('email').value;
     if (!regMail.test(mailId)) {
+        document.getElementById('submit').disabled = true;
         alert('Please enter valid email Id');
+    }
+    else {
+        enableSubmitButton();
     }
     localStorage.setItem("mailout", mailId);
 }
@@ -26,15 +48,26 @@ function PanValidation() {
     var regpan = /[A-Z]{5}[0-9]{4}[A-Z]{1}$/;
     var panId = document.getElementById('pan').value;
     if (!regpan.test(panId)) {
+        document.getElementById('submit').disabled = true;
         alert('Pan Number should be in the form: "AAAAA 1234 B" \nNOTE:no spaces allowed in between');
     }
+    else
+    {
+        enableSubmitButton();
+    }
+    
 }
 
 function PhoneValidation() {
     var regph = /^\d{11}$/;
     var phn = parseInt(document.getElementById('telephone').value);
     if (!regph.test(phn)) {
+        document.getElementById('submit').disabled = true;
         alert('Invalid Phone Number');
+    }
+    else
+    {
+        enableSubmitButton();
     }
 }
 
@@ -42,10 +75,15 @@ function LoanAmtValidation() {
     var regamt = /\d{1,9}$/;
     var lamt = document.getElementById("loanAmt").value;
     if (!regamt.test(lamt)) {
+        document.getElementById('submit').disabled = true;
         alert('please enter valid amount');
     }
     else if (lamt.length > 9) {
+        document.getElementById('submit').disabled = true;
         alert('maximum digits allwed are 9 digits');
+    }
+    else {
+    enableSubmitButton();
     }
 }
 
@@ -114,9 +152,10 @@ function CaptchaValidation()
         document.getElementById("captchamsg").innerHTML = "Captcha validation Successful";
         document.getElementById("captchamsg").style = "color:green";
         document.getElementById("result").innerHTML = "";
-       
+        enableSubmitButton();
     }
     else {
+        document.getElementById('submit').disabled = true;
         document.getElementById("captchamsg").innerHTML = "Try Again";
         document.getElementById("captchamsg").style = "color:red";
         document.getElementById("result").innerHTML = "";
