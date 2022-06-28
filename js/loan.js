07312c7ec711dec4f0b4ca6e4392c0fee4d707c4
Omkarfastcoder <99040673+Omkarfastcoder@users.noreply.github.com>
2022-06-28 13:12:22 +0530

    jshraegaergh

--- js/loan.js
+++ js/loan.js
@@ -128,34 +128,34 @@ if (n<100)
  else if(n>=100 && n<1000)
  {  if(convert(n%100).length > 0)
  {
-     ss= f2( Math.trunc(n/100)) + " hundred and  " + convert(n%100);
+     ss= f2( Math.trunc(n/100)) + " hundred  " + convert(n%100);
  }
   else
   {
-  ss= f2( Math.trunc(n/100)) + " hundred " + convert(n%100);
+  ss= f2( Math.trunc(n/100)) + " hundred  and" + convert(n%100);
   }
  }
  else if(n>=1000 && n<100000)
  {if(convert(n%100).length > 0  && convert(n%1000).length > 0)
  
     {
-        ss=f2( Math.trunc(n/1000)) + " thousand and"  + convert(n%1000);
+        ss=f2( Math.trunc(n/1000)) + " thousand "  + convert(n%1000);
     }
     else
     {
-        ss=f2( Math.trunc(n/1000)) + " thousand  "  + convert(n%1000);
+        ss=f2( Math.trunc(n/1000)) + " thousand  and "  + convert(n%1000);
 
     }
 }
    
 
  else if(n>=100000 && n<10000000)
- {if((convert(n%100).length > 0)  && (convert(n%1000).length > 0) && (convert(n%10000).length > 0))
+ {if((convert(n%100).length > 0)  && (convert(n%1000).length > 0) && (convert(n%100000).length > 0))
     {
-     ss=f2( Math.trunc(n/100000)) + " lakh  and" + convert(n%100000);
+     ss=f2( Math.trunc(n/100000)) + " lakh and " + convert(n%100000);
     }
     else{
-        ss=f2( Math.trunc(n/100000)) + " lakh  " + convert(n%100000);
+        ss=f2( Math.trunc(n/100000)) + " lakh " + convert(n%100000);
 
     }
      
@@ -168,7 +168,7 @@ if (n<100)
  
  }
  else{
-    ss=f2( Math.trunc(n/10000000)) + " crore  "+ convert(n%10000000);
+    ss=f2( Math.trunc(n/10000000)) + " crore "+ convert(n%10000000);
  }
  }
  
